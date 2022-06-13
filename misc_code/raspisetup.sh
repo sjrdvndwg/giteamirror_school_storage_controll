880bee69b918132817e9da78b73ab4ae5ebe64e0
sjrdvndwg <sjoerd.vandewege@gmail.com>
2022-06-13 11:23:39 +0200

    updated url links

--- misc_code/raspisetup.sh
+++ misc_code/raspisetup.sh
@@ -3,8 +3,8 @@
 apt install mysql-server node npm -y
 mkdir -p -v /server
 cd /server
-git clone https://gitea.puzzle-it.nu/Sjoerd/Shoppie.git &
-git clone https://gitea.puzzle-it.nu/Sjoerd/shoppie-backend.git &
+git clone https://gitea.puzzle-it.nu/School_projecten/Shoppie.git &
+git clone https://gitea.puzzle-it.nu/School_projecten/shoppie-backend.git &
 
 cd ./Shoppie && npm run dev &
 cd ./shoppie-backend && npm run dev &
